fafa93097941bdcb6faa002a6487e5dc4aa2ae99
Michael <michael@michael.com>
2020-09-22 14:06:26 +0200

    adding first file

--- 01_processes/src/abc.cpp
+++ 01_processes/src/abc.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <unistd.h>
 #include <chrono>
+#include <thread>
 
 using namespace std;
 
 int main() {
     auto pid{fork()};
-
+    // chrono::milliseconds sleeptime(1000);
     if (pid == 0) {
         while (true){
-            cout << "AD" << flush;
-            std::chrono::milliseconds sleeptime(1000); //sleep(1) doesn't work
+            cout << "X" << flush;
+            // std::chrono::milliseconds sleeptime(1000); //sleep(1) doesn't work
+            // this_thread::sleep_for(sleeptime);
+            fflush(stdout);
+            sleep(1000);
         }
     } else {
         while (true){
-            cout << "B" << flush;
-            std::chrono::milliseconds sleeptime(1000);
+            cout << "-" << flush;
+            // this_thread::sleep_for(sleeptime);
+            sleep(1000);
         }
     }
 }
