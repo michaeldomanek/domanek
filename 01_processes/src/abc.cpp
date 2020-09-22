0515fe6576aa25dbf36208ad04e29a751c5a8a1b
Michael <michael@michael.com>
2020-09-22 14:10:25 +0200

    back to previous version

--- 01_processes/src/abc.cpp
+++ 01_processes/src/abc.cpp
@@ -1,26 +1,19 @@
 #include <iostream>
 #include <unistd.h>
-#include <chrono>
-#include <thread>
 
 using namespace std;
 
 int main() {
     auto pid{fork()};
-    // chrono::milliseconds sleeptime(1000);
     if (pid == 0) {
         while (true){
-            cout << "X" << flush;
-            // std::chrono::milliseconds sleeptime(1000); //sleep(1) doesn't work
-            // this_thread::sleep_for(sleeptime);
-            fflush(stdout);
-            sleep(1000);
+            cout << "A" << flush;
+            sleep(1);
         }
     } else {
         while (true){
-            cout << "-" << flush;
-            // this_thread::sleep_for(sleeptime);
-            sleep(1000);
+            cout << "B" << flush;
+            sleep(1);
         }
     }
 }
