161a478e57eda4ce06232dfa5c0b8dd00b72fb85
Michael <michael@michael.com>
2020-09-26 16:41:41 +0200

    replace child with charout

--- 01_processes/src/abc.cpp
+++ 01_processes/src/abc.cpp
@@ -4,6 +4,8 @@
 #include <thread>
 #include <csignal>
 #include <cstdlib>
+#include <cerrno>
+#include <cstring>
 
 using namespace std;
 
@@ -14,19 +16,20 @@ int main() {
     chrono::milliseconds sleeptime(500);
 
     if (pid == 0) {
-        while (true){
-            cout << "A" << flush;
-            this_thread::sleep_for(sleeptime);
+        execl("./charout", "charout", "A", nullptr);
+        if (errno){
+            cout << endl;
+            kill(pid, SIGKILL);
+            exit(1);
         }
     } else {
         while (true){
             cout << "B" << flush;
             this_thread::sleep_for(sleeptime);
+            
             if (++counter == 6){
                 cout << endl;
                 kill(pid, SIGKILL);
-                // cout << pid << endl;
-                // sleep(10);
                 quick_exit(EXIT_SUCCESS);
             }
         }
