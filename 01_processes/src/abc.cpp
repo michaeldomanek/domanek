6ab6f0a18198b1e72b0653c8c7ff42c5fd80ba4e
Michael <michael@michael.com>
2020-09-26 14:27:15 +0200

    add chrono sleep

--- 01_processes/src/abc.cpp
+++ 01_processes/src/abc.cpp
@@ -1,19 +1,25 @@
 #include <iostream>
 #include <unistd.h>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
 int main() {
     auto pid{fork()};
+    auto counter = 0;
+    
+    chrono::milliseconds sleeptime(500);
+
     if (pid == 0) {
         while (true){
             cout << "A" << flush;
-            sleep(1);
+            this_thread::sleep_for(sleeptime);
         }
     } else {
         while (true){
             cout << "B" << flush;
-            sleep(1);
+            this_thread::sleep_for(sleeptime);
         }
     }
 }
