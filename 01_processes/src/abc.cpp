f1d4b9244f39ae2e42e9786b56afe8d0a52a1f46
Michael <michael@michael.com>
2020-09-28 11:13:33 +0200

    change code layout and add enviroment variables

--- 01_processes/src/abc.cpp
+++ 01_processes/src/abc.cpp
@@ -10,6 +10,19 @@
 
 using namespace std;
 
+void outputEnvironmentVariable(auto environmentVariableName){
+    const char* environmentVariable{getenv(environmentVariableName)};
+    if (environmentVariable){
+        execl("./charout", "charout", environmentVariable, nullptr);
+        if (errno){
+            cout << strerror(errno) << endl;
+            quick_exit(1);
+        }
+    } else {
+        cout << environmentVariableName << " doesn't exists!" << endl;
+    }
+}
+
 int main() {
     chrono::milliseconds sleeptime(3000);
 
@@ -17,26 +30,20 @@ int main() {
     auto pid2{fork()};
 
     if (pid == 0 && pid2 > 0) {
-        execl("./charout", "charout", "A", nullptr);
-        if (errno){
-            cout << strerror(errno) << endl;
-            quick_exit(1);
-        }
+        outputEnvironmentVariable("ABA_LETTER_A");
     } else if (pid > 0 && pid2 == 0) {
-        execl("./charout", "charout", "B", nullptr);
-        if (errno){
-            cout << strerror(errno) << endl;
-            quick_exit(1);
-        }
+        outputEnvironmentVariable("ABA_LETTER_B");
     } else if (pid > 0 && pid2 > 0) {
         int status;
         int status2;
 
         cout << "waiting for 3 seconds" << endl;
         this_thread::sleep_for(sleeptime);
+
         cout << endl << "killing both subprocesses with pids " << pid << " and " << pid2 << endl;
         kill(pid, SIGKILL);
         kill(pid2, SIGKILL);
+
         cout << "waiting for both subprocesses to be dead" << endl;
         waitpid(pid, &status, 0);
         waitpid(pid2, &status2, 0);
@@ -44,6 +51,5 @@ int main() {
         cout << "subprocess " << pid2 << " exited with " << WEXITSTATUS(status2) << endl;
 
         quick_exit(EXIT_SUCCESS);
-    }
-    
+    }    
 }
