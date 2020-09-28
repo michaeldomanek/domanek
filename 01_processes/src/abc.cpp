53b86fd3d7a7ca450bb8a642b8781139b48bfa70
Michael <michael@michael.com>
2020-09-28 10:50:53 +0200

    changing formatting

--- 01_processes/src/abc.cpp
+++ 01_processes/src/abc.cpp
@@ -5,7 +5,8 @@
 #include <csignal>
 #include <cstdlib>
 #include <cerrno>
-#include <string>
+#include <cstring>
+#include <sys/wait.h>
 
 using namespace std;
 
@@ -18,23 +19,30 @@ int main() {
     if (pid == 0 && pid2 > 0) {
         execl("./charout", "charout", "A", nullptr);
         if (errno){
-            cout << endl;
-            kill(pid, SIGKILL);
-            exit(1);
+            cout << strerror(errno) << endl;
+            quick_exit(1);
         }
     } else if (pid > 0 && pid2 == 0) {
         execl("./charout", "charout", "B", nullptr);
         if (errno){
-            cout << endl;
-            kill(pid2, SIGKILL);
-            exit(1);
+            cout << strerror(errno) << endl;
+            quick_exit(1);
         }
     } else if (pid > 0 && pid2 > 0) {
-        this_thread::sleep_for(sleeptime);
+        int status;
+        int status2;
 
-        cout << endl;
+        cout << "waiting for 3 seconds" << endl;
+        this_thread::sleep_for(sleeptime);
+        cout << endl << "killing both subprocesses with pids " << pid << " and " << pid2 << endl;
         kill(pid, SIGKILL);
         kill(pid2, SIGKILL);
+        cout << "waiting for both subprocesses to be dead" << endl;
+        waitpid(pid, &status, 0);
+        waitpid(pid2, &status2, 0);
+        cout << "subprocess " << pid << " exited with " << WEXITSTATUS(status) << endl;
+        cout << "subprocess " << pid2 << " exited with " << WEXITSTATUS(status2) << endl;
+
         quick_exit(EXIT_SUCCESS);
     }
     
