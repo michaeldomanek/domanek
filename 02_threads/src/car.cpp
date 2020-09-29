34efee5a26a6097aa058c8b890a6b8c688c471d2
MichaelDomanek <michaeldo.spam@gmail.com>
2020-09-29 19:13:35 +0200

    fixed output

--- 02_threads/src/car.cpp
+++ 02_threads/src/car.cpp
@@ -19,14 +19,14 @@ void Car::operator()() {
     ostringstream buf;
     std::random_device rd;
 
-    for (int i=0; i < 10; i++) {
+    for (int i=1; i <= 10; i++) {
         std::mt19937 gen{rd()};
         std::uniform_real_distribution<> dis{1, 10};
         
         double time = dis(gen);
         totalTime += time;
 
-        chrono::milliseconds sleeptime((int)time);
+        chrono::milliseconds sleeptime((int)time * 1000);
         this_thread::sleep_for(sleeptime);
 
         buf << i << " " << carName << " " << setprecision(3) << time << "s" << endl;
