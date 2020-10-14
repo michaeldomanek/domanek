c5038e8971ce8e8025c1402b89747b1813aee53c
Michael <michael@michael.com>
2020-10-14 09:15:48 +0200

    add cli parser

--- 03_sync1/src/main.cpp
+++ 03_sync1/src/main.cpp
@@ -1,16 +1,29 @@
 #include "account.h"
+#include "CLI11.hpp"
 
 #include <iostream>
 #include <thread>
 
 using namespace std;
 
-int main() {
-    Account acc{0};
+int main(int argc, char* argv[]) {
+    CLI::App app("Account app");
 
-    thread t1{Depositer{ref(acc), 6}};
-    thread t2{Depositer{ref(acc), 3}};
+    int balance{0};
+    app.add_option("balance", balance, "Initial balance")->required();
+    
+    int deposits{5};
+    app.add_option("-d,--deposits", deposits, "Count of deposits", true);
+    
+    CLI11_PARSE(app, argc, argv);
+
+    Account acc{balance};
+
+    thread t1{Depositer{ref(acc), deposits}};
+    thread t2{Depositer{ref(acc), deposits}};
 
     t1.join();
     t2.join();
+
+    cout << "Balance: " << acc.get_balance() << endl;
 }
