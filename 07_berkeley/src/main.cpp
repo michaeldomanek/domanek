98a6a0142b3ad8c4bc569c9c3be8fcb17111367d
Michael <michael@michael.com>
2020-12-31 12:51:25 +0100

    add TimeMaster

--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -21,11 +21,29 @@ class TimeSlave {
         }
 };
 
+class TimeMaster {
+    private:
+        string name;
+        Clock clock;
+    public:
+        TimeMaster(string name, int hours, int minutes, int seconds) {
+            this->name = name;
+            clock = Clock(name, hours, minutes, seconds);
+        }
+        void onStart() {
+            thread clockThread{ref(clock)};
+            clockThread.join();
+        }
+};
+
 int main() { //int argc, char* argv[]
     TimeSlave slave1{"slave1", 0, 0, 2};
     TimeSlave slave2{"slave2", 0, 2, 20};
+    TimeMaster master{"slave2", 0, 2, 0};
     thread t1{&TimeSlave::onStart, ref(slave1)};
     thread t2{&TimeSlave::onStart, ref(slave2)};
+    thread t3{&TimeMaster::onStart, ref(master)};
     t1.join();
     t2.join();
+    t3.join();
 }
